f1b07fc8b938504d14edab66afa0f3a0be5e0101
SOMTOCHUKWU <tonymars010@gmail.com>
2024-04-19 06:08:33 +0100

    updated app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [repos, setRepos] = useState([]);
 
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const response = await axios.get(
+          "https://github.com/Akuu426?tab=repositories"
+        );
+        setRepos(response.data);
+      } catch (error) {
+        console.error("Error fetching repositories:", error);
+      }
+    };
+
+    fetchRepos();
+  }, []);
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My GitHub Repositories</h1>
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <a href="https://github.com/Akuu426?tab=repositories">{repo.name}</a>
+            <p>{repo.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
     </>
-  )
-}
+  );
+};
 
 export default App
